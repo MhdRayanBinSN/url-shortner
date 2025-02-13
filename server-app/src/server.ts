42b13d278187f9d2d75b42aa0df1d8b62d101865
MhdRayanBinSN <rayan6203@gmail.com>
2025-02-13 19:39:42 +0530

    ts config

---
+++ server-app/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+
+const port = process.env.PORT || 5001;
+
+app.get('/',(req,res)=>{
+    res.json('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
